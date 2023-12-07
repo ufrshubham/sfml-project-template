5c0e10a8834edc3a89867b17e0f0c87aa32c580c
DevKage <ufrshubham@gmail.com>
2023-12-08 00:37:43 +0530

    Upgrade to SFML 2.6.1

--- include/Version.hpp
+++ include/Version.hpp
@@ -3,12 +3,12 @@
 
 const std::string &GetTemplateVersion()
 {
-    static std::string templateVersion("1.0");
+    static std::string templateVersion("2.0");
     return templateVersion;
 }
 
 const std::string &GetSFMLVersion()
 {
-    static std::string sfmlVersion("2.5.1");
+    static std::string sfmlVersion("2.6.1");
     return sfmlVersion;
 }
