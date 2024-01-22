5890123ffae04f84041982ff32f90637baa0a258
DevKage <ufrshubham@gmail.com>
2024-01-22 19:58:46 +0530

    Replace template image with auto-generate texts

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,26 @@
 #include <SFML/Graphics.hpp>
 #include <filesystem>
-#include <iostream> 
+#include <iostream>
 #include "Version.hpp"
 
 static void modifyCurrentWorkingDirectory();
+static void setupVersionTexts(sf::RenderWindow &window, sf::Font &font, sf::Text &templateVersion, sf::Text &sfmlVersion);
 
 int main()
 {
     modifyCurrentWorkingDirectory();
+
+    const auto clearColor = sf::Color(234, 240, 206);
     auto title = "Template-" + GetTemplateVersion() + "/SFML-" + GetSFMLVersion();
     sf::RenderWindow window(sf::VideoMode(640, 360), title, sf::Style::Close);
 
-    sf::Texture texture;
-    sf::Sprite sprite;
-    sf::Text text;
-    
-    if (texture.loadFromFile("resources/sfml-project-template.png"))
-    {
-        sprite.setTexture(texture);
-        sprite.setOrigin({(float)texture.getSize().x / 2, (float)texture.getSize().y / 2});
-        sprite.setPosition((float)window.getSize().x / 2, (float)window.getSize().y / 2);
-    }
-
     sf::Font font;
-    if (!font.loadFromFile("resources/OpenSans-Regular.ttf"))
+    sf::Text sfmlVersion;
+    sf::Text templateVersion;
+
+    if (font.loadFromFile("resources/FiraCode-Regular.ttf"))
     {
-        std::cerr << "Failed to load font. Application will continue without displaying text.\n";
-    }
-    else {
-        text.setFont(font);
-        text.setString("Hello SFML");
-        text.setCharacterSize(24);
-        text.setFillColor(sf::Color::White);
-        sf::FloatRect textRect = text.getLocalBounds();
-        text.setOrigin(textRect.left + textRect.width / 2.0f, textRect.top + textRect.height / 2.0f);
-        text.setPosition(sf::Vector2f(400.0f, 20.0f));
+        setupVersionTexts(window, font, templateVersion, sfmlVersion);
     }
 
     while (window.isOpen())
@@ -47,19 +33,50 @@ int main()
                 window.close();
             }
         }
-        
-        window.clear();
-        window.draw(sprite);
-        window.draw(text);
+
+        window.clear(clearColor);
+        window.draw(templateVersion);
+        window.draw(sfmlVersion);
         window.display();
     }
-    
+
     return 0;
+}
+
+void setupVersionTexts(sf::RenderWindow &window, sf::Font &font, sf::Text &templateVersion, sf::Text &sfmlVersion)
+{
+    auto windowCenter = sf::Vector2f(window.getSize().x * 0.5f, window.getSize().y * 0.5f);
+    const auto characterSize = 65;
+    const auto outlineThickness = 4.f;
+    const auto fillColor = sf::Color(63, 51, 77);
+    const auto outlineColor = sf::Color(192, 197, 193);
+
+    templateVersion.setFont(font);
+    templateVersion.setString("Template-v" + GetTemplateVersion());
+    templateVersion.setCharacterSize(characterSize);
+    templateVersion.setFillColor(fillColor);
+    templateVersion.setOutlineColor(outlineColor);
+    templateVersion.setOutlineThickness(outlineThickness);
+
+    auto textRect = templateVersion.getLocalBounds();
+    templateVersion.setOrigin(textRect.left + textRect.width * 0.5f, textRect.top + textRect.height * 0.5f);
+    templateVersion.setPosition(windowCenter - sf::Vector2f(0.f, textRect.height));
+
+    sfmlVersion.setFont(font);
+    sfmlVersion.setString("SFML-v" + GetSFMLVersion());
+    sfmlVersion.setCharacterSize(characterSize);
+    sfmlVersion.setFillColor(fillColor);
+    sfmlVersion.setOutlineColor(outlineColor);
+    sfmlVersion.setOutlineThickness(outlineThickness);
+
+    textRect = sfmlVersion.getLocalBounds();
+    sfmlVersion.setOrigin(textRect.left + textRect.width * 0.5f, textRect.top + textRect.height * 0.5f);
+    sfmlVersion.setPosition(windowCenter + sf::Vector2f(0.f, textRect.height));
 }
 
 void modifyCurrentWorkingDirectory()
 {
-    while (!std::filesystem::exists("resources")) 
+    while (!std::filesystem::exists("resources"))
     {
         std::filesystem::current_path(std::filesystem::current_path().parent_path());
     }
