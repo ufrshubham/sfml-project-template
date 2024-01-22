b5e83e1a018d386b39c8cc343a9659790dee62fc
Devrim <106804411+therealvoric@users.noreply.github.com>
2024-01-22 13:14:56 +0100

    Deleted Experimental in line 66

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ void modifyCurrentWorkingDirectory()
 {
     while (!std::filesystem::exists("resources")) 
     {
-        std::filesystem::current_path(std::experimental::filesystem::current_path().parent_path());
+        std::filesystem::current_path(std::filesystem::current_path().parent_path());
     }
     auto cwd = std::filesystem::current_path();
 }
